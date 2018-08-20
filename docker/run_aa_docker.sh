a70c18f8153acf0c13e2ee99371fd1fe08556cdf
Nam-phuong Nguyen <namphuon@notifier@genomemaster.ucsd.edu>
2018-08-20 08:39:35 -0700

    making docker branch

---
+++ docker/run_aa_docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+INPUT_DIR=$1
+BAM_FILE=$2
+BED_FILE=$3
+OUT_DIR=$4
+OUT_NAME=$5
+OPTIONS=$6
+
+docker run --rm -it -e OPTIONS="$OPTIONS" -e BED_FILE=$BED_FILE -e BAM_FILE=$BAM_FILE -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $INPUT_DIR:/home/input -v $OUT_DIR:/home/output aa 
---
+++ docker/run_aa_script.sh
@@ -0,0 +1,1 @@
+python programs/AmpliconArchitect/src/AmpliconArchitect.py --bed /home/input/$BED_FILE --bam /home/input/$BAM_FILE --out /home/output/$OUT_NAME $OPTIONS
