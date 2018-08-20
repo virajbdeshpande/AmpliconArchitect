d8c9695bb4abb0d3f512081d93aa05baa8b11983
Nam-phuong Nguyen <namphuon@notifier@genomemaster.ucsd.edu>
2018-08-20 10:13:39 -0700

    Updated docker to pull from repository

--- docker/run_aa_script.sh
+++ docker/run_aa_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $AA_DATA_REPO
-python programs/AmpliconArchitect/src/AmpliconArchitect.py --bed /home/input/$BED_FILE --bam /home/input/$BAM_FILE --out /home/output/$OUT_NAME $OPTIONS
+python programs/AmpliconArchitect-master/src/AmpliconArchitect.py --bed /home/input/$BED_FILE --bam /home/input/$BAM_FILE --out /home/output/$OUT_NAME $OPTIONS
