37a9b4c0811fb2fae51be9d59065eec6f5f0428c
Nam-phuong Nguyen <namphuon@notifier@genomemaster.ucsd.edu>
2018-08-20 09:19:38 -0700

    finalizing docker

--- docker/run_aa_docker.sh
+++ docker/run_aa_docker.sh
@@ -6,4 +6,6 @@ OUT_DIR=$4
 OUT_NAME=$5
 OPTIONS=$6
 
-docker run --rm -it -e OPTIONS="$OPTIONS" -e BED_FILE=$BED_FILE -e BAM_FILE=$BAM_FILE -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $INPUT_DIR:/home/input -v $OUT_DIR:/home/output aa 
+#docker run --rm -it -e OPTIONS="$OPTIONS" -e BED_FILE=$BED_FILE -e BAM_FILE=$BAM_FILE -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $INPUT_DIR:/home/input -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
+
+docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE=$BED_FILE -e BAM_FILE=$BAM_FILE -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $INPUT_DIR:/home/input -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
--- docker/run_aa_script.sh
+++ docker/run_aa_script.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+echo $AA_DATA_REPO
 python programs/AmpliconArchitect/src/AmpliconArchitect.py --bed /home/input/$BED_FILE --bam /home/input/$BAM_FILE --out /home/output/$OUT_NAME $OPTIONS
