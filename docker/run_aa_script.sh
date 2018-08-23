8740c15112d1091a3ea4e53210f7e3986bef2ce7
virajbdeshpande <virajbdeshpande@gmail.com>
2018-08-22 20:48:39 -0700

    Docker data_repo env variable

--- docker/run_aa_script.sh
+++ docker/run_aa_script.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-export AA_DATA_REPO=/home/data_repo
+AA_DATA_REPO=/home/data_repo
+export AA_DATA_REPO
+ls $AA_DATA_REPO
 python programs/AmpliconArchitect-master/src/AmpliconArchitect.py --bed $BED_FILE --bam $BAM_FILE --out $OUT_PREFIX $OPTIONS
