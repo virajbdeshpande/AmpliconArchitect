82f90648fd5e9befc73d01fc785e9d3298f4b64e
virajbdeshpande <virajbdeshpande@gmail.com>
2018-08-23 08:51:50 -0700

    Docker mosek license file path

--- docker/run_aa_docker.sh
+++ docker/run_aa_docker.sh
@@ -2,8 +2,7 @@
 BAM_FILE=`readlink -f "$1"`
 BED_FILE=`readlink -f "$2"`
 OUT_PREFIX=`readlink -f "$3"`
-MOSEK_LICENSE_FOLDER=$4
-OPTIONS=$5
+OPTIONS=$4
 BAM_DIR=`dirname "$BAM_FILE"`
 BED_DIR=`dirname "$BED_FILE"`
 OUT_DIR=`dirname "$OUT_PREFIX"`
@@ -12,4 +11,4 @@ BED_FILENAME=`basename "$BED_FILE"`
 OUT_NAME=`basename "$OUT_PREFIX"`
 
 
-docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE="/home/bed_dir/$BED_FILENAME" -e BAM_FILE="/home/bam_dir/$BAM_FILENAME" -e OUT_PREFIX="/home/output/$OUT_NAME" -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh -v $MOSEK_LICENSE_FOLDER:/home/programs/mosek/8/licenses
+docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE="/home/bed_dir/$BED_FILENAME" -e BAM_FILE="/home/bam_dir/$BAM_FILENAME" -e OUT_PREFIX="/home/output/$OUT_NAME" -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh -v $MOSEKLM_LICENSE_FILE:/home/programs/mosek/8/licenses
