8ecfbb21e77ab3808bed469776a7edadd8322bed
virajbdeshpande <virajbdeshpande@gmail.com>
2018-08-22 20:42:52 -0700

    Docker file path bugfixes

--- docker/run_aa_docker.sh
+++ docker/run_aa_docker.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-BAM_FILE=`readlink -f $1`
-BED_FILE=`readlink -f $2`
-OUT_PREFIX=`readlink -f $3`
+BAM_FILE=`readlink -f "$1"`
+BED_FILE=`readlink -f "$2"`
+OUT_PREFIX=`readlink -f "$3"`
 OPTIONS=$4
-BAM_DIR=`dirname $BAM_FILE`
-BED_DIR=`dirname $BED_FILE`
-OUT_DIR=`dirname $OUT_PREFIX`
-BAM_FILENAME=`basename $BAM_FILE`
-BED_FILENAME=`basename $BED_FILE`
-OUT_NAME=`basename $OUT_PREFIX`
+BAM_DIR=`dirname "$BAM_FILE"`
+BED_DIR=`dirname "$BED_FILE"`
+OUT_DIR=`dirname "$OUT_PREFIX"`
+BAM_FILENAME=`basename "$BAM_FILE"`
+BED_FILENAME=`basename "$BED_FILE"`
+OUT_NAME=`basename "$OUT_PREFIX"`
 
 
-docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE=/home/bed_dir/$BED_FILENAME -e BAM_FILENAME=/home/bam_dir/$BAM_FILENAME -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
+docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE="/home/bed_dir/$BED_FILENAME" -e BAM_FILENAME="/home/bam_dir/$BAM_FILENAME" -e OUT_PREFIX="/home/output/$OUT_NAME" -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
--- docker/run_aa_script.sh
+++ docker/run_aa_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo $AA_DATA_REPO
-python programs/AmpliconArchitect-master/src/AmpliconArchitect.py --bed /home/input/$BED_FILENAME --bam /home/input/$BAM_FILENAME --out /home/output/$OUT_NAME $OPTIONS
+python programs/AmpliconArchitect-master/src/AmpliconArchitect.py --bed $BED_FILE --bam $BAM_FILE --out $OUT_PREFIX $OPTIONS
