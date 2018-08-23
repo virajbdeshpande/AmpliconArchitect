edc2001ceac7321bee1fe6e0e4802348c0cc4663
virajbdeshpande <virajbdeshpande@gmail.com>
2018-08-22 20:27:48 -0700

    Docker load bam and bed directly from path

--- docker/run_aa_docker.sh
+++ docker/run_aa_docker.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-INPUT_DIR=$1
-BAM_FILE=$2
-BED_FILE=$3
-OUT_DIR=$4
-OUT_NAME=$5
-OPTIONS=$6
+BAM_FILE=`readlink -f $1`
+BED_FILE=`readlink -f $2`
+OUT_PREFIX=`readlink -f $3`
+OPTIONS=$4
+BAM_DIR=`dirname $BAM_FILE`
+BED_DIR=`dirname $BED_FILE`
+OUT_DIR=`dirname $OUT_PREFIX`
+BAM_FILENAME=`basename $BAM_FILE`
+BED_FILENAME=`basename $BED_FILE`
+OUT_NAME=`basename $OUT_PREFIX`
 
-#docker run --rm -it -e OPTIONS="$OPTIONS" -e BED_FILE=$BED_FILE -e BAM_FILE=$BAM_FILE -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $INPUT_DIR:/home/input -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
+echo $BAM_DIR $BED_DIR $OUT_DIR
 
-docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE=$BED_FILE -e BAM_FILE=$BAM_FILE -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $INPUT_DIR:/home/input -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
+
+docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILENAME=$BED_FILENAME -e BAM_FILENAME=$BAM_FILENAME -e OUT_NAME=$OUT_NAME -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh
