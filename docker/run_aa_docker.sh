f2b70e60cf3581a1475116b8b9e079aa8d62113b
virajbdeshpande <virajbdeshpande@gmail.com>
2018-08-23 13:44:12 -0700

    Docker mosek.lic import

--- docker/run_aa_docker.sh
+++ docker/run_aa_docker.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# BAM_FILE=`readlink -f "$1"`
-# BED_FILE=`readlink -f "$2"`
-# OUT_PREFIX=`readlink -f "$3"`
-# OPTIONS=$4
-# BAM_DIR=`dirname "$BAM_FILE"`
-# BED_DIR=`dirname "$BED_FILE"`
-# OUT_DIR=`dirname "$OUT_PREFIX"`
-# BAM_FILENAME=`basename "$BAM_FILE"`
-# BED_FILENAME=`basename "$BED_FILE"`
-# OUT_NAME=`basename "$OUT_PREFIX"`
 
 bam_set=False
 bed_set=False
@@ -21,21 +11,29 @@ for i in "$@"; do
         BAM_FILE=`readlink -f "$i"`
         BAM_DIR=`dirname "$BAM_FILE"`
         BAM_FILENAME=`basename "$BAM_FILE"`
-        argstring="$argstring /home/bam_dir/$BAM_FILENAME"
+        BAM_ARG=$(printf %q "/home/bam_dir/$BAM_FILENAME")
+        argstring="$argstring $BAM_ARG"
     elif [ "$bed_set" == "True" ]
     then
         bed_set=False
         BED_FILE=`readlink -f "$i"`
         BED_DIR=`dirname "$BED_FILE"`
         BED_FILENAME=`basename "$BED_FILE"`
-        argstring="$argstring /home/bed_dir/$BED_FILENAME"
+        BED_ARG=$(printf %q "/home/bed_dir/$BED_FILENAME")
+        argstring="$argstring $BED_ARG"
     elif [ "$out_set" == "True" ]
     then
         out_set=False
         OUT_PREFIX=`readlink -f "$i"`
         OUT_DIR=`dirname "$OUT_PREFIX"`
         OUT_NAME=`basename "$OUT_PREFIX"`
-        argstring="$argstring /home/output/$OUT_NAME"
+        OUT_ARG=$(printf %q "/home/output/$OUT_NAME")
+        # OUT_ARG=\"/home/output/$OUT_NAME\"
+        #echo "$OUT_ARG"
+        #echo $argstring
+        argstring="$argstring $OUT_ARG"
+        #echo $argstring
+        #echo "$argstring"
     elif [ "$i" == "--bam" ]
     then
         bam_set=True
@@ -52,11 +50,6 @@ for i in "$@"; do
         argstring="$argstring $i"
     fi
 done
-
-echo $argstring
-
-
-# docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e OPTIONS="$OPTIONS" -e BED_FILE="/home/bed_dir/$BED_FILENAME" -e BAM_FILE="/home/bam_dir/$BAM_FILENAME" -e OUT_PREFIX="/home/output/$OUT_NAME" -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh -v $MOSEKLM_LICENSE_FILE:/home/programs/mosek/8/licenses
 
 
 docker run --rm -it -e AA_DATA_REPO=/home/data_repo -e argstring="$argstring" -v $AA_DATA_REPO:/home/data_repo -v $BAM_DIR:/home/bam_dir -v $BED_DIR:/home/bed_dir -v $OUT_DIR:/home/output aa sh /home/run_aa_script.sh -v $MOSEKLM_LICENSE_FILE:/home/programs/mosek/8/licenses
--- docker/run_aa_script.sh
+++ docker/run_aa_script.sh
@@ -4,4 +4,4 @@ export AA_DATA_REPO
 MOSEKLM_LICENSE_FILE=/home/programs/mosek/8/licenses
 export MOSEKLM_LICENSE_FILE
 
-python programs/AmpliconArchitect-master/src/AmpliconArchitect.py --bed $BED_FILE --bam $BAM_FILE --out $OUT_PREFIX $OPTIONS
+python programs/AmpliconArchitect-master/src/AmpliconArchitect.py $argstring
