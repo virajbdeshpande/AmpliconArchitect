df52bd8d7b394b6f41f73b253bb780e9fc784240
virajbdeshpande <virajbdeshpande@gmail.com>
2018-08-23 08:03:25 -0700

    Docker mosek licnse file runtime

--- docker/run_aa_script.sh
+++ docker/run_aa_script.sh
@@ -2,6 +2,7 @@
 AA_DATA_REPO=/home/data_repo
 export AA_DATA_REPO
 ls $AA_DATA_REPO
-export MOSEKLM_LICENSE_FILE=$PWD/mosek/8/licenses
+MOSEKLM_LICENSE_FILE=$PWD/mosek/8/licenses
+export MOSEKLM_LICENSE_FILE
 
 python programs/AmpliconArchitect-master/src/AmpliconArchitect.py --bed $BED_FILE --bam $BAM_FILE --out $OUT_PREFIX $OPTIONS
