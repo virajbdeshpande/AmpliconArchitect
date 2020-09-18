4088a0238ad3af2a86722b2c3c8f3a4e4b40730e
Jens Luebeck <jluebeck@eng.ucsd.edu>
2020-09-18 12:30:26 -0700

    temp logging

--- src/amplified_intervals.py
+++ src/amplified_intervals.py
@@ -103,6 +103,11 @@ if args.bam != "":
     coverage_stats_file.close()
     bamFileb2b = b2b.bam_to_breakpoint(bamFile, coverage_stats=cstats)
     rdList = hg.interval_list([r for r in rdList if float(r.info[-1]) > GAIN + 2 * max(1.0, bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0]) - 2])
+    for r in rdList:
+        print(r.chrom,r.start,r.end)
+        print(r.info)
+        print(bamFileb2b.median_coverage(refi=r)[0] / bamFileb2b.median_coverage()[0] -2)
+
 
 genome_features = hg.oncogene_list
 amplicon_listl = rdList
