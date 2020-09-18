8d731f3c827c894605373b895d1abf4095147dce
Jens Luebeck <jluebeck@eng.ucsd.edu>
2020-09-18 12:20:12 -0700

    better logging of bad ref pos

--- src/bam_to_breakpoint.py
+++ src/bam_to_breakpoint.py
@@ -287,7 +287,8 @@ class bam_to_breakpoint():
                         continue
                     newpos = hg.absPos(cwindow.chrom, ((cwindow.end + cwindow.start) / 2) - 5000)
                 if hg.chrPos(newpos) is None:
-                    logging.debug("Unable to locate reference position: " + str(newpos) + " " + str(sumchrLen))
+                    logging.debug("Unable to locate reference position: " + refi.chrom + " " + str(refi.start) + " "
+                                 + str(refi.end) + " " + str(newpos) + " " + str(sumchrLen))
                     iteri+=1
                     continue
                 (c,p) = hg.chrPos(newpos)
@@ -326,7 +327,8 @@ class bam_to_breakpoint():
                         continue
                     newpos = hg.absPos(cwindow.chrom, ((cwindow.end + cwindow.start) / 2) - 5000)
                 if hg.chrPos(newpos) is None:
-                    logging.warning("Unable to locate reference position: " + str(newpos) + " " + str(sumchrLen))
+                    logging.warning("Unable to locate reference position: " + refi.chrom + " " + str(refi.start) + " "
+                                 + str(refi.end) + " " + str(newpos) + " " + str(sumchrLen))
                     iteri+=1
                     continue
                 (c,p) = hg.chrPos(newpos)
