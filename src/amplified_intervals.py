e26fa4f24737fa70adf88fb184c51185bd68a0f7
Jens Luebeck <jluebeck@eng.ucsd.edu>
2020-09-18 10:55:44 -0700

    add shebang

--- src/amplified_intervals.py
+++ src/amplified_intervals.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 # This software is Copyright 2017 The Regents of the University of California. All Rights Reserved. Permission to copy, modify, and distribute this software and its documentation for educational, research and non-profit purposes, without fee, and without a written agreement is hereby granted, provided that the above copyright notice, this paragraph and the following three paragraphs appear in all copies. Permission to make commercial use of this software may be obtained by contacting:
 #
 # Office of Innovation and Commercialization
